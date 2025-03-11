8551a7337cfee1d87f82e3dc9474007461bf6a1c
B12_309_20\b12s309 <santiagosotoalvarez266@gmail.com>
2025-03-11 11:51:29 -0500

    creamos clases para agregar y finalisamos agregando alistas

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -1,1 +1,39 @@
-# TODO: Add code here
+# TODO
+from operator import truediv
+
+
+class Todo:
+    def __int__(self, code_id: int, title: str, description: str):
+     self.code_id: int = code_id
+     self.title: str = title
+     self.description: str = description
+     self.completed: bool = False
+     self.tags: list[str] = []
+
+    def mark_completed(self):
+        self.completed = True
+        return self.completed
+
+    def add_tag(self, tag):
+        if tag is not self.tags:
+            self.tags.append(tag)
+
+    def __str__(self):
+        return str(f"{self.code_id} - {self.title}")
+
+class TodoBook:
+
+    def __int__(self):
+        self.todos: dict[int,Todo] = {}
+
+    def __add__(self, title: str, description: str):
+        id: int = len (self.todos)
+        valor = Todo (id, title, description) # crear el objecto
+        self.todos[id] = valor
+
+
+
+
+
+
+
